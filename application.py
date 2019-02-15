4655f7cd8f4d807876fa7196f684bedfaaab56c4
Richmond Rice <richmond@ricey.org>
2019-02-15 19:35:36 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('bender.html')
-	
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+	return('blah')
+	#return render_template('bender.html')
+
+'''
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+'''
