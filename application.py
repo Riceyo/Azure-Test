a5ea06b2292d3442072e82505e8765c73d3073ad
Richmond Rice <richmond@ricey.org>
2019-02-15 19:30:57 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello"
-
-@app.route('/test')
+@app.route('/')
 def index():
    return render_template('bender.html')
 	
