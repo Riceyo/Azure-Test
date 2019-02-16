411222c2fb72cf8d6b3237384c723067ef2cd28c
Richmond Rice <richmond@ricey.org>
2019-02-16 14:34:53 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
+import secrets
 
 app = Flask(__name__)
 
-@app.route('/')
+def random_quote:
+	quote_list = ["Bite my shiny, metal ass!"]
+	quote_list.append = ["Hey sexy mama, wanna kill all humans?"]
+	quote_list.append = ["You know what cheers me up? Other people's misfortune."]
+	quote_list.append = ["I'm so embarrassed. I wish everyone else was dead!"]
+	return secrets.choice(quote_list)
+
+@app.route("/")
 def index():
-	return render_template('bender.html')
+	return render_template("bender.html", quote = random_quote())
 
 '''
 if __name__ == "__main__":
