73ddeec5d8e5e931cfd6fe62479f97d042102078
Richmond Rice <richmond@ricey.org>
2019-02-16 15:12:13 +0000

    Update application.py

--- application.py
+++ application.py
@@ -9,6 +9,11 @@ def random_quote():
 	quote_list.append("Hey sexy mama, wanna kill all humans?")
 	quote_list.append("You know what cheers me up? Other people's misfortune.")
 	quote_list.append("I'm so embarrassed. I wish everyone else was dead!")
+	quote_list.append("Shut up baby, I know it!")
+	quote_list.append("Hot diggity daffodil!")
+	quote_list.append("I came to this planet with a simple dream of killing all humans.")
+	quote_list.append("I'm going to make my own theme park. With blackjack! And hookers!")
+	quote_list.append("I say the world must learn of our peaceful ways... BY FORCE!")
 	return secrets.choice(quote_list)
 
 @app.route("/")
